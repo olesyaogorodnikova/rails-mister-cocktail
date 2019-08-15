8726093f1c4ebfa5837d5d75f757867c8a89c496
olesya ogorodnikova <ooleszja@gmail.com>
2019-08-15 12:40:59 +0200

    added seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'json'
+require 'open-uri'
+
+Ingredient.destroy_all
+Cocktail.destroy_all
+Dose.destroy_all
+
+url = 'http://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+ing = open(url).read
+ingredients = JSON.parse(ing)
+
+ingredients["drinks"].each do |ingredient|
+  Ingredient.create(name: ingredient["strIngredient1"])
+
+end
+
+url = 'http://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Alcoholic'
+cock = open(url).read
+cocktails = JSON.parse(cock)
+
+cocktails["drinks"].each do |cocktail|
+  Cocktail.create(name: cocktail["strDrink"])
+end
