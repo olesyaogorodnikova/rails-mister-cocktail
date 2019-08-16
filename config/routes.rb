90d5420eb8c11111b8f89a504956a798dc52066b
olesya ogorodnikova <ooleszja@gmail.com>
2019-08-16 11:59:50 +0200

    changes home

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,5 @@
 Rails.application.routes.draw do
-  get 'doses/new'
-  get 'doses/create'
-  get 'cocktails/index'
-  get 'cocktails/show'
-  get 'cocktails/new'
-  get 'cocktails/create'
-  get 'cocktails/destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :cocktails, only: [:index, :show, :new, :create, :destroy] do
     resources :doses, only: [:new, :create, :edit]
